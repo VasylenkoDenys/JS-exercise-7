0f79b3e154ce60a9e466add5ac689d80370dcafb
Denys <109107950+VasylenkoDenys@users.noreply.github.com>
2022-09-27 14:59:25 +0300

    error correction

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /*Реализовать класс RangeValidator. Обьекты, созданные им должны обладать следующими свойcтвами:
 from(число)
 to(число)
@@ -12,31 +12,49 @@ Bonus tasks:
 Создать проверки которые убедятся, что число во from меньше числа to и наоборот.
 Создать проверки, которые помогут избежать попадание неправильных типов данных в from и to.*/
 
-
 class RangeValidator {
-  constructor (from, to){
-    if (typeof from !== 'number' && to !== 'number') {
-      throw TypeError('Must be a number');
-    } 
-    if (from >= to || to <= from){
-      throw RangeError('Not in the range');
-    }
-// debugger;
+  #from;
+  #to;
+  constructor(from, to) {
     this.from = from;
     this.to = to;
   }
-  get range(){
-    return Array(this.to - this.from + 1).fill().map((_, i) => this.from + i)
+  get from() {
+    return this.#from;
   }
-  validate(number){
-    if (number < this.from || number > this.to){
-    throw RangeError('Not in the range');
+  get to(){
+    return this.#to;
   }
-    if (typeof number !== 'number'|| isNaN(number)){
-      throw TypeError('Must be a number');
+  get range() {
+    return [this.from, this.to];
   }
-  return number;
+  set from(from){
+    if (typeof from !== "number" || isNaN(from)) {
+      throw TypeError("Must be a number");
+    }
+    if (from >= this.to) {
+      throw RangeError("Not in the range");
+    }
+    this.#from = from;
+  }
+  set to(to){
+    if (typeof to !== "number" || isNaN(to)) {
+      throw TypeError("Must be a number");
+    }
+    if (to <= this.from) {
+      throw RangeError("Not in the range");
+    }
+    this.#to = to;
+  }
+  validate(number) {
+    if (typeof number !== "number" || isNaN(number)) {
+      throw TypeError("Must be a number");
+    }
+    if (number < this.from || number > this.to) {
+      throw RangeError("Not in the range");
+    }
+    return number;
   }
 }
 
-const obj1 = new RangeValidator (5, 50);
+const obj1 = new RangeValidator(50, 55);
